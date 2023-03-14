76bdbd284f85e94611c7ea59b00375bb58a9d903
Maxi Ruiz Diaz <maxruizdz@hotmail.com>
2023-03-14 17:53:45 -0300

    Modelo a nivel basico

--- Controllers/EscuelaController.cs
+++ Controllers/EscuelaController.cs
@@ -1,13 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using platzi_asp_net_core.Models;
+
 namespace platzi_asp_net_core.Controllers
 {
     public class EscuelaController: Controller
     {
         public IActionResult Index() {
 
+            Escuela escuela = new Escuela() ;
+            escuela.AñodeFundacion = 2005;
+            escuela.EscuelaID = Guid.NewGuid().ToString();
+            escuela.Nombre = "Roberto Muller";
 
-
-            return View();
+            return View(escuela);
         }
 
 
---
+++ Models/Escuela.cs
@@ -0,0 +1,12 @@
+﻿namespace platzi_asp_net_core.Models
+{
+    public class Escuela
+    {
+        public String EscuelaID { get; set; }
+        public string Nombre { get; set; }
+
+
+        public int AñodeFundacion { get; set; }
+     
+    }
+}
