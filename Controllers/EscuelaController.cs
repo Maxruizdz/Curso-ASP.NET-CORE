23c2dbd55666fc2e8f3b68949fd829694b353364
Maxi Ruiz Diaz <maxruizdz@hotmail.com>
2023-03-14 10:42:51 -0300

    Creacion vista y Controlador Escuela

---
+++ Controllers/EscuelaController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace platzi_asp_net_core.Controllers
+{
+    public class EscuelaController: Controller
+    {
+        public IActionResult Index() {
+
+
+
+            return View();
+        }
+
+
+    }
+}
