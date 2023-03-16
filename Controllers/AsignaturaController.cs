e74b0869a3bbc79faef1cb57e46e31d3b3e897fb
Maxi Ruiz Diaz <maxruizdz@hotmail.com>
2023-03-16 16:04:52 -0300

    Vistas parciales para listar asignaturas

--- Controllers/AsignaturaController.cs
+++ Controllers/AsignaturaController.cs
@@ -6,11 +6,38 @@ namespace platzi_asp_net_core.Controllers
     public class AsignaturaController: Controller
     {
         public IActionResult Index() {
-            Asignatura asignatura = new Asignatura();
-            asignatura.Nombre = "Biologia";
 
 
-            return View(asignatura);
+            return View(new Asignatura
+            {
+                Nombre = "Programacion",
+                UniqueId = Guid.NewGuid().ToString()
+            });
+        }   
+        public IActionResult MultiAsignatura() {
+
+            var listaAsignaturas = new List<Asignatura>() {
+        new Asignatura {
+        Nombre = "Matemáticas",
+        UniqueId = Guid.NewGuid ().ToString ()
+        },
+        new Asignatura {
+        Nombre = "Educación Física",
+        UniqueId = Guid.NewGuid ().ToString ()
+        },
+        new Asignatura {
+        Nombre = "Castellano",
+        UniqueId = Guid.NewGuid ().ToString ()
+        },
+        new Asignatura {
+        Nombre = "Ciencias Naturales",
+        UniqueId = Guid.NewGuid ().ToString ()
+        },
+        new Asignatura {
+        Nombre = "Programacion",
+        UniqueId = Guid.NewGuid ().ToString ()
+        } };
+            return View("MultiAsignatura",listaAsignaturas);
 
         }
     }
